33b71d4b6a1e22e39363a074f23b6a282bb42a73
Gojeflone <avak1908@gmail.com>
2017-10-30 11:55:54 -0400

    Made prompt class

--- src/game/Prompt.java
+++ src/game/Prompt.java
@@ -3,5 +3,8 @@ package game;
 import javax.swing.JPanel;
 
 public class Prompt extends JPanel{
+	//Prompt gets put on top of the view when a selection needs to be made
+	//Prompt has to options to click in. Yes or No on whether the object belongs. 
+	//It'll be a rough outline with a picture of the object inside
 		
 }
