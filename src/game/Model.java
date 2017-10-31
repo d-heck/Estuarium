bc2110405e20dde07936d2c5794e0178f5897136
Jason Hickman <Jason@wifi-roaming-128-4-80-184.host.udel.edu>
2017-10-30 23:01:06 -0400

    stuff

--- src/game/Model.java
+++ src/game/Model.java
@@ -7,6 +7,9 @@ public class Model {
 	 *  		- Should ask the player if organism belongs or not.
 	 *  		- Should cycle through every organism in the level list.
 	 */
+	
+	
+	
 	public static void main(String[] Args) {
 		Level x = new Level();
 		x.play();
